019005019821b2c625446086ea10d43747b23fa2
김태민 <gtaemin254@gmail.com>
2025-09-11 23:42:57 +0900

    크루목록 #4

---
+++ static/js/crew/event.js
@@ -0,0 +1,9 @@
+const moreBtn = document.querySelector(".css-1x7qv6q");
+moreBtn.addEventListener("click", (e) => {
+    console.log(11111);
+});
+
+const categoryBtn = document.querySelector(".css-ygosan");
+categoryBtn.addEventListener("click", (e) => {
+    console.log("클릭");
+});
