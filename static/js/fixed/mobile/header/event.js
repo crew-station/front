032681048705c1cd56029ccd16621e2c03c2f2d8
정충효 <alzkdpf000@gmail.com>
2025-09-16 01:44:27 +0900

    fixed/mobile/header login profile edit #69

---
+++ static/js/fixed/mobile/header/event.js
@@ -0,0 +1,122 @@
+// 서브헤더 클릭 이벤트
+const subHeaders = document.querySelectorAll(
+    "div.sticky-container.sticky-sub-container"
+);
+subHeaders.forEach((subHeader) => {
+    subHeader.addEventListener("click", (e) => {
+        console.log(subHeader);
+
+        const activeLink = e.target.closest("a.item-link.navigation-item-link");
+        if (activeLink) {
+            const aLinks = subHeader.querySelectorAll(
+                "a.item-link.navigation-item-link"
+            );
+            aLinks.forEach((aLink) => {
+                aLink.classList.remove("active");
+            });
+            activeLink.classList.add("active");
+        }
+    });
+});
+
+// 스크롤 이벤트
+const header = document.getElementById("header");
+const secondaryHeaders = document.querySelectorAll(
+    "div.layout-navigation-secondary"
+);
+
+window.addEventListener("wheel", (e) => {
+    if (e.wheelDeltaY < 0) {
+        secondaryHeaders.forEach((secondaryHeader) => {
+            console.log(secondaryHeader);
+
+            secondaryHeader.classList.add("scroll-down");
+            secondaryHeader.style.top = "-41px";
+        });
+    } else {
+        secondaryHeaders.forEach((secondaryHeader) => {
+            secondaryHeader.classList.remove("scroll-down");
+            secondaryHeader.style.top = "51px";
+        });
+    }
+});
+
+// 검색 아이콘 클릭 이벤트
+
+const searchIcon = document.querySelector("button.search-icon");
+const searchModal = document.querySelector("div.search-icon-modal");
+const closeSearchModal = document.querySelector("button.search-cancle");
+searchIcon.addEventListener("click", (e) => {
+    !searchModal.classList.contains("open") &&
+        searchModal.classList.add("open");
+});
+
+closeSearchModal.addEventListener("click", (e) => {
+    searchModal.classList.contains("open") &&
+        searchModal.classList.remove("open");
+});
+
+// 메뉴 리스트에서 버튼 클릭 이벤트
+const menuBtns = document.querySelectorAll("button.menu-item-btn");
+const crewBtn = document.querySelector("div.menu-list-crew");
+const diaryBtn = document.querySelector("div.menu-list-diary");
+let clickCheck = document.querySelector("button.menu-item-btn.gift");
+menuBtns.forEach((menuBtn) => {
+    menuBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        console.log(12312);
+        console.log(clickCheck);
+        if (clickCheck) {
+            // clickCheck.removeAttribute("style");
+            clickCheck.lastElementChild.classList.remove("active");
+        }
+
+        if (clickCheck === menuBtn) {
+            menuBtn.nextElementSibling &&
+                (menuBtn.nextElementSibling.style.height = "0px");
+            clickCheck = null;
+            return;
+        }
+        clickCheck = menuBtn;
+        console.log(clickCheck);
+        menuBtn.lastElementChild.classList.add("active");
+        if (menuBtn.classList.contains("crew")) {
+            crewBtn.style.height = "172px";
+            diaryBtn.style.height = "0px";
+        } else if (menuBtn.classList.contains("diary")) {
+            diaryBtn.style.height = "129px";
+            crewBtn.style.height = "0px";
+        } else {
+            crewBtn.style.height = "0px";
+            diaryBtn.style.height = "0px";
+        }
+    });
+});
+
+const detailBtns = document.querySelectorAll("li.detail-link-item");
+let clickDetailBtn = null;
+detailBtns.forEach((detailBtn) => {
+    detailBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        console.log(clickDetailBtn);
+        if (clickDetailBtn) {
+            console.log(clickDetailBtn);
+            clickDetailBtn.removeAttribute("style");
+        }
+        clickDetailBtn = detailBtn;
+        detailBtn.style.backgroundColor = "rgb(247, 249, 250)";
+    });
+});
+
+// 메뉴 모달 열기
+const modalUp = document.querySelector("button.list-action-btn");
+modalUp.addEventListener("click", (e) => {
+    modalDown.classList.add("open");
+});
+// 메뉴 모달 닫기
+const modalDown = document.querySelector("div.menu-list-modal");
+modalDown.addEventListener("click", (e) => {
+    console.log(123);
+
+    modalDown.classList.remove("open");
+});
