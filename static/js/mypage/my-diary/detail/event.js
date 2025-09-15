cfaf914dc1c3748c535cb54e6bcf6de332adb6e0
ccoccooo <ccocco55@gmail.com>
2025-09-15 10:31:10 +0900

    fin close #46

--- static/js/mypage/my-diary/detail/event.js
+++ static/js/mypage/my-diary/detail/event.js
@@ -16,6 +16,34 @@ secretToggle.addEventListener("click", (e) => {
     }
 });
 
+// 이미지 팝업
+
+const targets = document.querySelectorAll(".target");
+const images = document.querySelectorAll(".diary-img");
+const modal = document.querySelector(".img-modal");
+const modalImg = document.querySelector(".img-modal img");
+const closeBtn = document.querySelector(".img-close-button");
+
+targets.forEach((target, index) => {
+    target.addEventListener("click", () => {
+        const imgSrc = images[index].getAttribute("src");
+        modalImg.setAttribute("src", imgSrc);
+        modal.style.display = "flex";
+    });
+});
+
+// 닫기 버튼
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// 모달 바깥 클릭하면 닫기 (선택)
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
 // 신고하기 버튼
 
 const reportBtn = document.querySelector(".report-button");
